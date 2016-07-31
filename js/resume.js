185d898fc28bbf9b50c0231b5a60779e351ceb1d
majiangl <06rjgcmjl@gmail.com>
2016-07-31 18:42:27 +0800

    update

---
+++ js/resume.js
@@ -0,0 +1,9 @@
+(function($){
+    $(function(){
+        var tempFn = doT.template($('#resumeTpl').remove().text());
+        $.get('resume.json',function(data){
+            var resultText = tempFn(data);
+            $('body').html(resultText);
+        });
+    });
+}(jQuery));
