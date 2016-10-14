d1dce18ca35de06914a72eb9d24a7142fdf070de
马江龙 <06rjgcmjl@gmail.com>
2016-10-14 17:08:53 +0800

    Update resume.js

--- js/resume.js
+++ js/resume.js
@@ -3,7 +3,7 @@
         var tempFn = doT.template($('#resumeTpl').remove().text());
         $.get('resume.json',function(data){
             var resultText = tempFn(data);
-            $('body').html(resultText);
+            $('body').html(resultText).append('<input>');
         });
     });
 }(jQuery));
